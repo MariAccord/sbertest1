c9807dad42bef4524456d710334caaf835a1db21
MariAccord <allergi-ya@yandex.ru>
2020-08-20 12:13:50 +0300

    add logging

--- src/main/java/ru/allergiya/sbertest1/controller/IncrementController.java
+++ src/main/java/ru/allergiya/sbertest1/controller/IncrementController.java
@@ -1,5 +1,7 @@
 package ru.allergiya.sbertest1.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -7,6 +9,7 @@ import ru.allergiya.sbertest1.service.IncrementService;
 
 @RestController
 public class IncrementController {
+    private static Logger LOG = LoggerFactory.getLogger(IncrementController.class);
     private IncrementService incrementService;
 
     public IncrementController(IncrementService incrementService) {
@@ -15,11 +18,18 @@ public class IncrementController {
 
     @GetMapping("/increment")
     public ResponseEntity<String> incrementNumbers(@RequestParam String numbers){
-        return new ResponseEntity<>(incrementService.increment(numbers), HttpStatus.OK);
+        LOG.info("increment request got, numbers : {}", numbers);
+        try {
+            return new ResponseEntity<>(incrementService.increment(numbers), HttpStatus.OK);
+        } catch (RuntimeException e) {
+            LOG.error("Error invoking incrementNumbers {}", e.getCause());
+            return new ResponseEntity<>(HttpStatus.UNSUPPORTED_MEDIA_TYPE);
+        }
     }
 
     @PostMapping(value = "/setdiff")
     public ResponseEntity<?> setDiffNumber(@RequestBody int diff){
+        LOG.info("Got setdiff request, new diff value : {}", diff);
         incrementService.setDiff(diff);
         return new ResponseEntity<>(HttpStatus.OK);
     }
--- src/main/java/ru/allergiya/sbertest1/service/IncrementService.java
+++ src/main/java/ru/allergiya/sbertest1/service/IncrementService.java
@@ -1,12 +1,16 @@
 package ru.allergiya.sbertest1.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import ru.allergiya.sbertest1.controller.IncrementController;
 
 import javax.annotation.PostConstruct;
 import java.util.Arrays;
 
 @Service
 public class IncrementService {
+    private static Logger LOG = LoggerFactory.getLogger(IncrementService.class);
     //можно вынести в конфиг, но в рамках данной задачи оставляем так
     private static final int DEFAULT_DIFF = 1;
     private static final String IN_DELIMETER = ",";
@@ -18,14 +22,19 @@ public class IncrementService {
         diff = DEFAULT_DIFF;
     }
 
-    public String increment(String numbers) {
+    public String increment(String numbers) throws RuntimeException {
         String[] numbersArray = numbers.split(IN_DELIMETER);
         StringBuilder sb = new StringBuilder();
-        Arrays.stream(numbersArray)
-                //т.к. Integer.MAX + 1 = Integer.MIN, следует использовать BigInteger
-                //в рамках задачи BigInteger использовать не будем
-                .map(s -> Integer.parseInt(s.trim()) + diff)
-                .forEach(num -> sb.append(num).append(OUT_DELIMETER));
+        try {
+            Arrays.stream(numbersArray)
+                    //т.к. Integer.MAX + 1 = Integer.MIN, следует использовать BigInteger
+                    //в рамках задачи BigInteger использовать не будем
+                    .map(s -> Integer.parseInt(s.trim()) + diff)
+                    .forEach(num -> sb.append(num).append(OUT_DELIMETER));
+        } catch (NumberFormatException e) {
+            LOG.error("Error parsing string numbers : {}", numbers);
+            throw new RuntimeException(e);
+        }
         sb.delete(sb.length()-2, sb.length());
         return sb.toString();
     }
